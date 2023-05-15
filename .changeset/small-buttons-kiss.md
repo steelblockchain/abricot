5b051be2ed1a538dea242bb6d31864ebd839f7b7
root <root@DESKTOP-B4V30I0.localdomain>
2023-05-16 00:02:34 +0200

    i forgot something ='(

---
+++ .changeset/small-buttons-kiss.md
@@ -0,0 +1,5 @@
+---
+"abricot": patch
+---
+
+i forgot something
--- src/dofus-module/packet.ts
+++ src/dofus-module/packet.ts
@@ -5,8 +5,12 @@ export default class Dofus2PacketAnalyzer {
     private readonly reader: ReaderBigEndianStream =
         new ReaderBigEndianStream();
 
-    analyze(buffer: Buffer, client_side: boolean): Array<Dofus2Packet> {
-        this.reader.add(buffer, buffer.length);
+    analyze(
+        buffer: Buffer,
+        data_length: number,
+        client_side: boolean
+    ): Array<Dofus2Packet> {
+        this.reader.add(buffer, data_length);
         const result: Array<Dofus2Packet> = [];
 
         while (this.reader.remnant_size() > 0) {
